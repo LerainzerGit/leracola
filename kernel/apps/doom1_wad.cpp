2c157b43e73bc86135f6aac5d7b558c344f90c39
LerainzerGit <therecroommoviebackintime@gmail.com>
2026-08-19 20:17:30 +0100

    Create doom1_wad.cpp

---
+++ kernel/apps/doom1_wad.cpp
@@ -0,0 +1,10 @@
+// Generated definition file containing actual binary WAD data
+#include "doom1_wad.h"
+
+namespace leraOS::Apps {
+    extern "C" const unsigned char doom1_wad[] = {
+        0x49, 0x57, 0x41, 0x44, // "IWAD" header bytes...
+        // ... rest of WAD payload generated via xxd -i
+    };
+    extern "C" const unsigned int doom1_wad_len = sizeof(doom1_wad);
+}
