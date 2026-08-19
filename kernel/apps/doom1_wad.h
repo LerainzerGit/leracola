9c94f3d8259f573fc216e68bedfddd7029c96345
LerainzerGit <therecroommoviebackintime@gmail.com>
2026-08-19 20:16:44 +0100

    Create doom1_wad.h

---
+++ kernel/apps/doom1_wad.h
@@ -0,0 +1,7 @@
+// Embedded Shareware DOOM WAD Declaration
+#pragma once
+
+namespace leraOS::Apps {
+    extern "C" const unsigned char doom1_wad[];
+    extern "C" const unsigned int doom1_wad_len;
+}
